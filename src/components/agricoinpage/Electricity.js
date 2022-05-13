8e0cb7fcaf6c2fad29ecf3b0413edbcde0b5d2b7
Rohit0035 <rohitdevelopersveltosest@gmail.com>
2022-05-13 18:53:58 +0530

    rohitelec

--- src/components/agricoinpage/Electricity.js
+++ src/components/agricoinpage/Electricity.js
@@ -5,36 +5,28 @@ import Cloth from "../../assets/img/Cloth.jpg";
 import electrimg from '../../assets/img/electrimg.png';
 import { Container, Row,  Col, Input, InputGroup, Form, Button } from "reactstrap";
 import axios from "axios";
+import swal from "sweetalert";
 // import swal from 'sweetalert';
 class Electricity extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: "",
-      re_type: "",
-      MobileNo: 8121787777,
-      APIKey: "vzfWPhGe8GQRWHarKgzFVXJYxmkgFLdZrUG",
-      REQTYPE: "BILLPAY",
-      SERCODE: "",
-      CUSTNO: "",
-      REFMOBILENO: "",
-      AMT: "",
-      STV: "",
-      FIELD1: "",
-      FIELD2: "",
-      FIELD3: "",
-      FIELD4: "",
-      FIELD5: "",
-      PCODE: 500081,
-      LAT: 17.3617,
-      LONG: 78.4751,
-      RESPTYPE: "",
-      serviceR:[]
+       code :"MAE",
+      account: "",
+      AMTNO:'',
+      responseData : {},
+      
+  
+      
+      billAmount:"",
+  
+      serviceR:[],
+     
   };
 }
 componentDidMount() {
   axios
-    .get("http://35.154.134.118/api/admin/getEleOperators")
+    .get("http://35.154.134.118:8000/admin/electricity_operator/")
     .then((response) => {
     console.log(response.data.data);
     this.setState({ serviceR: response.data.data});
@@ -44,27 +36,92 @@ componentDidMount() {
       });
     }
     changeHandler = (e) => {
+      let inputtxt = 0;
       this.setState({ [e.target.name]: e.target.value });
-    };
-    submitHandler = (e) => {
-      e.preventDefault();
-        axios
-          .post(`http://35.154.134.118/api/admin/Elec_Recharge/`, this.state)
-          .then((response) => {
-            console.log(response.data);
-            // console.log(response.data.STATUSMSG);
-            // if(response.data.STATUSMSG !== "Failed" && response.data.STATUSMSG !== "Failed" ){
-            //   swal("Success!", "Recharge SuccessFull!", "success");
-            // }
-            // else {
-            //   swal("Error!", "Recharge UnsuccssFull!", "error");
-            // }
-          })
-        .catch((error) => {
-          console.log(error.response);
-    });
-  }
+      
+      if(e.target.name === 'AMTNO'){
+        inputtxt = e.target.value;
+
+        if (/^[1234][0-9]{9}$/.test(inputtxt)) {
+          this.setState({ isE_Bill : false, number : inputtxt });
+          
+          let payload = {
+            number : inputtxt
+          }
+   
+  //   axios
+  //   .post(`http://35.154.134.118:8000/admin/elec_paybill`)
+  //   .then((response) => {
+  //     console.log(response.data.data);
+  //     console.log(response.data.STATUSMSG);
+  //     if(response.data.STATUSMSG !== "Failed" && response.data.STATUSMSG !== "Failed" ){
+  //        swal("Success!", "Recharge SuccessFull!", "success");
+  //      }
+  //      else {
+  //        swal("Error!", "Recharge UnsuccssFull!", "error");
+  //      }
+  //   })
+  // .catch((error) => {
+  //   console.log(error.response);
+  //  });
+axios
+.post(`http://35.154.134.118:8000/admin/fetch_Bill/`,payload)
+.then((response) => {
+  console.log(response.data.data);
+   console.log(response.data.STATUSMSG);
+   this.setState({ responseData : response.data,billAmount:response.data?.state })
+   
+
+
+})
+.catch((error) => {
+console.log(error.response);
+});
+
+return true;
+}else{
+  this.setState({ isE_Bill : true });        
+  return false;
+}}
+  };
+  onRechargeSubmit = (e) =>{
+    if(this.state.AMTNO === '' || this.state.AMTNO === 0 ){
+     this.setState({ isAmountVal : true });
+     return true;
+    } else{
+     let payload = {
+       
+       walletId :this.state.userData.walletId,
+         amount : this.state.AMTNO, //parseInt(this.state.number)
+         biller_code : this.state.responseData?.code,
+         number : this.state.number //parseInt(this.state.number)
+     
+     }
+     axios
+      .post(`http://35.154.134.118:8000/admin/fetch_Bill
+      `, payload)
+      
+      .then((response) => {
+        console.log(response.data);
+        console.log(response.data.STATUSMSG);
+        this.setState({ responseData : response.data })
+        swal("Successful!", "Recharge Successful!", "success");
+        this.props.history.push("/orderrecharge"); 
+      })
+      
+      .catch((error) => {
+        
+        console.log(error.response);
+        swal("Error!", "Invalid!", "error");
+      });
+       }
+      } 
+
+    updateItem = (value) => {
+       console.log('Selected Value::',value); 
+    } 
 render() {
+
   return (
     <LayoutOne>
     <Container fluid className="mb-4">
@@ -117,27 +174,28 @@ render() {
                     className="form-control"
                     placeholder="Consumer Number"
                   />
+                   <span style={{color : 'red'}}>{this.state.isE_Bill ? 'Please enter 10 digits IVRS number' : null}</span>
                 </Col>
                 <small style={{color:"red",}}>(Subscriber ID starts with 1 and is 10 digits long. To locate it, press the home button on remote.)</small>
                 <br></br>
                 <br></br>
                 <Col md="12">
-                  <InputGroup>
-                    <span className="sr-2">Rs.</span>
-                      <Input 
+                <InputGroup>
+                <span className="sr-2">{this.state.isAmount ?'Rs': null}</span>
+                  
+                      <Input
                         className="form-control"
                         placeholder="Amount"
-                        name="AMT"
-                        type="number"
-                        required
-                        value={this.state.AMT}
-                        onChange={this.changeHandler}
+                        name="AMTNO"
+                        onChange={this.changeHandlerAmount}
                       />
+                    
+            
                   </InputGroup>
                 </Col>
                 <br></br>
                   {localStorage.getItem("auth-token") ? (
-                  <Button className="sr-btn">Proceed </Button>
+                  <Button className="sr-btn" onClick ={this.onRechargeSubmit}>Proceed </Button>
                   ) : (
                   <Button 
                     className="sr-btn"
