99441125063ccc136610c9747211baf6a185ab0f
Rohit0035 <rohitdevelopersveltosest@gmail.com>
2022-11-05 18:56:51 +0530

    nrew change

--- src/pages/shop-product/ProductSticky.js
+++ src/pages/shop-product/ProductSticky.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+
 import React, { Fragment, Component } from "react";
 import MetaTags from "react-meta-tags";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
@@ -6,10 +6,9 @@ import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import "../../assets/css/productdetail.css";
-//import RelatedProductSlider from "../../wrappers/product/RelatedProductSlider";
 import ProductDescriptionTab from "../../wrappers/product/ProductDescriptionTab";
 import ProductImageDescriptionSticky from "../../wrappers/product/ProductImageDescriptionSticky";
-import axiosConfig from "../../axiosConfig";
+import Axios from "axios";
 
 export class ProductSticky extends Component {
   constructor(props) {
@@ -23,29 +22,22 @@ export class ProductSticky extends Component {
 
   componentDidMount() {
     let { id } = this.props.match.params;
-    axiosConfig.get(`/admin/getoneproduct/${id}`)
+       Axios.get(`http://44.205.32.29/admin/getoneproduct/${id}`)
       .then((response) => {
-        console.log(response.data.data);
         this.setState({
           detail: response.data.data,
           pid: response.data.data._id,
         });
       })
-      .catch((error) => {
-        console.log(error.response);
-      });
+      .catch((error) => {});
   }
 
   render() {
-    //console.log(this.state.detail);
     return (
       <Fragment>
         <MetaTags>
           <title>NEXUS PAY</title>
-          <meta
-            name="description"
-            content="Product page of flone react minimalist eCommerce template."
-          />
+          
         </MetaTags>
 
         <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>
@@ -70,30 +62,11 @@ export class ProductSticky extends Component {
             productFullDesc={JSON.stringify(this.state.detail)}
             productid={this.state.pid}
           />
-
-          {/* related product slider */}
-          {/* <RelatedProductSlider
-            spaceBottomclassName="pb-95"
-            category={product.category[0]}
-          /> */}
         </LayoutOne>
       </Fragment>
     );
   }
 }
 
-// ProductSticky.propTypes = {
-//   location: PropTypes.object,
-//   product: PropTypes.object
-// };
-
-// const mapStateToProps = (state, ownProps) => {
-//   const itemId = ownProps.match.params.id;
-//   return {
-//     product: state.productData.products.filter(
-//       single => single.id === itemId
-//     )[0]
-//   };
-// };
-
 export default ProductSticky;
+
