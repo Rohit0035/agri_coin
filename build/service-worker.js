344daf7b6451df4e55d7ef44ae540eb5eccdc4fb
Rohit0035 <rohitdevelopersveltosest@gmail.com>
2022-11-07 11:31:10 +0530

    build 11/7/2002

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62ac3c6a99bc33292b9ab75e80f18643.js"
+  "/precache-manifest.d1ae9dd581dc59f746139c337bbf92b9.js"
 );
 
 self.addEventListener('message', (event) => {
